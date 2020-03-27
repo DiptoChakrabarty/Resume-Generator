2184f79dc16d4c838917bdbbe8806e221cc20da8
Dipto Chakrabarty <diptochuck123@gmail.com>
2020-03-27 18:48:32 +0530

    initial

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from datetime import datetime
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+
+app =  Flask(__name__)
+
+app.config['SECRET_KEY'] = 'e23739c67eade607c64f90c3ebb479ca' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' 
