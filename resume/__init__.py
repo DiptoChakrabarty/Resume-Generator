47812e534aee0f57ee28cb08a9f342dbcb268a1f
DiptoChakrabarty <diptochuck123@gmail.com>
2020-07-19 18:07:29 +0530

    removed print

--- resume/__init__.py
+++ resume/__init__.py
@@ -23,7 +23,6 @@ app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = os.environ["MAIL_USERNAME"]
 app.config["MAIL_PASSWORD"] = os.environ["PASSWORD"]
-print("***********",os.environ["MAIL_USERNAME"],"////////////////",os.environ["PASSWORD"])
 mail = Mail(app)
 
 db = SQLAlchemy(app)
